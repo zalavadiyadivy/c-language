9ca33003043fa1f0f3b7c89fb0d01a948c953465
zalavadiyadivy <162704921+zalavadiyadivy@users.noreply.github.com>
2024-04-10 20:42:43 +0530

    11.1

---
+++ 11.1Q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int *p,i,n,a[20];
+    printf("Enter Size: ");
+    scanf("%d",&n);
+    
+    printf("Enter Elements:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",*p*(*p));
+    }
+    p=&a;
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d",*p*(*p));
+    }
+    return 0;
+}
