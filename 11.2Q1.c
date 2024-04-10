d4b29ca6b1e960d7600bbcbb091808ac466bd2c1
zalavadiyadivy <162704921+zalavadiyadivy@users.noreply.github.com>
2024-04-10 20:45:13 +0530

    11.2

---
+++ 11.2Q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void reverseArray(int *arr, int size) {
+    int *start = arr;
+    int *end = arr + size - 1; 
+    
+    while (start < end) {
+    
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+
+        end--;
+    }
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nOriginal array:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    reverseArray(arr, size);
+
+    printf("\nReversed array:\n");
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
